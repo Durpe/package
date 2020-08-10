786269ea3772ba7b53e75c07c6bdab41d1a2fbaa
Durpe <47975576+Durpe@users.noreply.github.com>
2020-08-10 15:07:35 +0800

    Update package.py

--- package.py
+++ package.py
@@ -16,18 +16,18 @@ class Window(object):
         self.software_box()
 
     def software_box(self):
-        self.btn1=Button(self.application_window, text='中望(QT位置)',width=10,command=lambda :self.copy_qt(name="中望(QT位置)"))
+        self.btn1=Button(self.application_window, text='test',width=10,command=lambda :self.copy_qt(name="test"))
         self.btn1.grid(row=1, column=1, padx=55, pady=5, ipadx=10, ipady=5)
-        self.btn2=Button(self.application_window, text='CREO(QT位置)',width=10, command=lambda :self.copy_qt(name="CATIA(QT位置)"))
+        self.btn2=Button(self.application_window, text='test',width=10, command=lambda :self.copy_qt(name="test"))
         self.btn2.grid(row=2, column=1, padx=55, pady=5, ipadx=10, ipady=5)
-        self.btn3=Button(self.application_window, text='CATIA(QT位置)',width=10, command=lambda :self.copy_qt(name="CREO(QT位置)"))
+        self.btn3=Button(self.application_window, text='test',width=10, command=lambda :self.copy_qt(name="test"))
         self.btn3.grid(row=3, column=1, padx=55, pady=5, ipadx=10, ipady=5)
 
-        self.btn4 = Button(self.application_window, text='中望(API位置)',width=10,command=lambda :self.copy_api(name="中望(API位置)"))
+        self.btn4 = Button(self.application_window, text='test',width=10,command=lambda :self.copy_api(name="test"))
         self.btn4.grid(row=1, column=2, padx=10, pady=5,ipadx=10, ipady=5)
-        self.btn5 = Button(self.application_window, text='CREO(API位置)',width=10,command=lambda :self.copy_api(name="CATIA(API位置)"))
+        self.btn5 = Button(self.application_window, text='test',width=10,command=lambda :self.copy_api(name="test"))
         self.btn5.grid(row=2, column=2, padx=10, pady=5,ipadx=10, ipady=5)
-        self.btn6 = Button(self.application_window, text='CATIA(API位置)',width=10,command=lambda :self.copy_api(name="CREO(API位置)"))
+        self.btn6 = Button(self.application_window, text='test',width=10,command=lambda :self.copy_api(name="test"))
         self.btn6.grid(row=3, column=2, padx=10, pady=5,ipadx=10, ipady=5)
         self.application_window.mainloop()
 
@@ -36,7 +36,7 @@ class Window(object):
     def copy_qt(self,name):
         # 选择路径
         address = filedialog.askdirectory(parent=self.application_window, initialdir="C:", title="请选择%s的路径" %name)
-        file_name = ["Qt5Gui.dll", "Qt5Core.dll", "Qt5Sql.dll", "Qt5Svg.dll", "Qt5Widgets.dll"]
+        file_name = ["", "", "", "", ""]
         #folder_name = ["platforms", "imageformats", "plugins"]
         try:
             if address != '':
